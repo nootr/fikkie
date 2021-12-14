4d98f34eaafde48414ffa77f34ceb272996e00f9
Joris Hartog <jorishartog@hotmail.com>
2021-12-14 15:19:31 +0100

    Create config template on first run

--- fikkie/config.py
+++ fikkie/config.py
@@ -9,6 +9,41 @@ BASE_DIR = os.getenv('FIKKIE_BASE_DIR', os.path.expanduser('~/.fikkie'))
 CONFIG_FILE = os.getenv('FIKKIE_CONFIG', os.path.join(BASE_DIR, 'config.yaml'))
 BROKER_DIR = os.getenv('FIKKIE_BROKER_DIR', os.path.join(BASE_DIR, 'broker'))
 
+CONFIG_TEMPLATE = """---
+## SSH config
+# Fikkie needs to know which user to use to login into the servers. This will
+# default to "fikkie".
+#
+# Example:
+#
+# ssh:
+#   username: fikkie
+
+## Servers
+# This is where you specify the commands fikkie needs to execute over SSH to
+# test them.
+#
+# Example:
+#
+# servers:
+#   jhartog.dev:
+#     - command: 'curl -s -o /dev/null -w "%{http_code}" jhartog.dev'
+#       expected: '200'
+#     - command: 'sudo systemctl status mariadb | grep "Active: active" -c'
+#       expected: '1'
+
+## Notifiers
+# If you want fikkie to notify state changes/problems, you'll need to specify
+# the notifiers here.
+#
+# Example:
+#
+# notifiers:
+#   - type: telegram
+#     token: 'foo'
+#     chat_id: 1234567
+"""
+
 needed_dirs = [
     BASE_DIR,
     BROKER_DIR,
@@ -21,13 +56,24 @@ for directory in needed_dirs:
         logging.info(f"Created directory: {directory}")
 
 
-def load_config(filename):
+def create_config_template(filename) -> None:
+    """Create a config template at the given location."""
+    try:
+        with open(filename, 'x') as f:
+            f.write(CONFIG_TEMPLATE)
+    except Exception as e:
+        logging.error(f"Could not write config template at {filename}: {e}")
+        exit(1)
+
+
+def load_config(filename: str) -> dict:
     try:
         with open(filename, 'r') as f:
             return yaml.safe_load(f)
     except FileNotFoundError as e:
-        logging.error(f"Could not find config file: {e}")
-        exit(1)
+        create_config_template(filename)
+        logging.warning(f"Created a config template at {filename}.")
+        exit(0)
     except yaml.YAMLError as e:
         logging.error(f"Could not parse config: {e}")
         exit(1)
