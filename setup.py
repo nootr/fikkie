e0d5f721ce4e8a57e4008364eb57f6f4713f43dd
Joris Hartog <jorishartog@hotmail.com>
2021-12-16 13:39:59 +0100

    Bump version to v0.2.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setup(
     name="fikkie",
-    version="0.2.0",
+    version="0.2.1",
     description="Lightweight monitoring tool",
     license="MIT",
     long_description=long_description,
